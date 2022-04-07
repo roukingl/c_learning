24b600ca7dbbc521b933b6f35a8774dff2ad193c
gitee_drh <2062806593@qq.com>
2022-04-07 21:04:25 +0800

    C

---
+++ test_4_6/test_4_6/test.c
@@ -0,0 +1,93 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("%15d", 0xABCDEF);
+//
+//    return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%#o %#X\n", 1234, 1234);
+//	return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int i = 0;
+//    char ch = 0;
+//    float f = 0;
+//    scanf("%c %d %f", &ch, &i, &f);
+//
+//    printf("%c %d %.6f", ch, i, f);
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("%d%8d%8d", a, b, c);
+//
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%x %o", &a, &b);
+//
+//    printf("%d", a + b);
+//
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//    printf("%d", a + b);
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//
+//    printf("%d", a / b);
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//
+//void main()
+//{
+//	int a = 65;
+//	char c = 'A';
+//	printf("%x,%d", a, c);
+//}
---
+++ test_4_7/test_4_7/test.c
@@ -0,0 +1,28 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    float f = 0;
+//    int t = 0;
+//    scanf("%f", &f);
+//
+//    t = f;
+//    printf("%d",  t% 10);
+//
+//
+//    return 0;
+//}
+//
+//
+//
+//# include <stdio.h>
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    a = a / 10;
+//    printf("%d", a % 10);
+//    return 0;
+//}
