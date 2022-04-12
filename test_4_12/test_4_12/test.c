65636278340fd0a6a8158afe89296ae29bfc244c
gitee_drh <2062806593@qq.com>
2022-04-12 22:02:42 +0800

    4_12的代码

---
+++ test_4_12/test_4_12/test.c
@@ -0,0 +1,98 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("Hello World!\n");
+//	return 0;
+//}
+
+
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    float f = 0;
+//    scanf("%f", &f);
+//    if (2 * f > (int)(2 * f))
+//    {
+//        printf("%d\n", (int)f + 1);
+//    }
+//    else
+//    {
+//        printf("%d\n", (int)f);
+//    }
+//
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    float f = 0;
+//    scanf("%f", &f);
+//
+//    printf("%d", (int)(f + 0.5));
+//
+//    return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i;
+//	float fl;
+//	char ch;
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int wight = 20;//全局变量
+//
+//int main()
+//{
+//	int light = 40;//局部变量
+//	int wight = 80;//重新定义全局变量
+//	printf("wight is %d\nlight is %d", wight, light);
+//
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int num1 = 0;
+//    int num2 = 0;
+//    int sum = 0;
+//    printf("输入两个操作数:>");
+//    scanf("%d %d", &num1, &num2);//可以在控制台控制num1, num2变量的输入
+//    sum = num1 + num2;//num1与num2相加放入sum中
+//    printf("sum = %d\n", sum);//打印sum
+//
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	float f = 0;
+//	f = 3.14;
+//	for (i = 1; i <= 100; i = i + 2)
+//	{
+//			printf("%d ", i);
+//	}
+//	printf("%f", f);
+//	return 0;
+//}
+
