a5bc47601433f19be1e86c856d153c4ad3d831ca
gitee_drh <2062806593@qq.com>
2022-04-12 22:01:30 +0800

    4_10号的c代码

---
+++ test_4_10/test_4_10/test.c
@@ -0,0 +1,56 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    double f = 0;
+//    scanf("%lf", &f);
+//    double c = (5 / 9.0) * (f - 32);
+//    printf("%.3lf", c);
+//
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int r = 0;
+//    scanf("%d", &r);
+//    double s = 3.14 * r * r;
+//    printf("%lf", s);
+//
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int r1, r2;
+//    scanf("%d %d", &r1, &r2);
+//    printf("%lf", 1.0 / (1.0 / r1 + 1.0 / r2));
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int h, r;
+//    scanf("%d %d", &h, &r);
+//    float v = 3.14 * h * r * r;
+//    float p = (int)10 / v;
+//
+//    if (10000.0 / v == p)
+//    {
+//        printf("%d", 10000.0 / v);
+//    }
+//    else
+//    {
+//        printf("%d", (int)(10000.0 / v) + 1);
+//    }
+//
+//    return 0;
+//}
