bb4bda46f8e4abb0648dde0f975015c530895cd1
gitee_drh <2062806593@qq.com>
2022-04-24 21:49:30 +0800

    4_24号的代码

---
+++ test_4_24/test_4_24/test.c
@@ -0,0 +1,56 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//    char ch[10000] = { 0 };
+//    char word[22] = { 0 };
+//    gets(ch);
+//    int i = strlen(ch) - 1;
+//    int j = 0;
+//
+//    while (i != 0)
+//    {
+//        while ((ch[i] >= 65 && ch[i] <= 90) || (ch[i] < 122 && ch[i] >97))
+//        {
+//            i--;
+//        }
+//        strcpy(word, &ch[i + 1]);
+//        puts(word);
+//        printf(" ");
+//        ch[i] = '\0';
+//        i--;
+//    }
+//
+//    return 0;
+//}
+//
+//
+//int main(void)
+//{
+//	int i = 0;
+//	char ch[100][22] = { 0 };
+//	for (i = 0; i < 100; i++)
+//	{
+//		gets(ch[i]);
+//	}
+//	for (; i >= 0; i--)
+//	{
+//		puts(ch[i]);
+//		
+//	}
+//
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int arr[const 11] = { 0 };
+	return 0;
+}
