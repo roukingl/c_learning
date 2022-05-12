ca8a942e93d7c7c8ddce641f089ec427e7ccc64c
gitee_drh <2062806593@qq.com>
2022-05-12 20:40:58 +0800

    5_12的代码

---
+++ test_5_12/test_5_12/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("男生扣1，女生扣0:");
+//	scanf("%d", &a);
+//
+//	//如果满足a == 1则执行第一个，满足a == 0则执行第二个
+//	//如果第一个和第二个都不满足则执行第三个
+//	if (a == 1)
+//	{
+//		printf("靓仔！");
+//	}
+//	else if (a == 0)
+//	{
+//		printf("小姐姐！");
+//	}
+//	else
+//	{
+//		printf("你是什么性别我也不知道哎");
+//	}
+//
+//	return 0;
+//}
+////
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//
+//	if (a > 0)
+//		if (a == 10)
+//			printf("a等于10");
+//		else
+//			printf("a小于0");
+//		
+//
+//	return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int day = 0;
+//    scanf("%d", &day);
+//    switch (day)
+//    {
+//    case 1:
+//        printf("星期一\n");
+//    case 2:
+//        printf("星期二\n");
+//    case 3:
+//        printf("星期三\n");
+//    case 4:
+//        printf("星期四\n");
+//    case 5:
+//        printf("星期五\n");
+//    case 6:
+//        printf("星期六\n");
+//    case 7:
+//        printf("星期天\n");
+//    default:
+//        printf("输入错误，请重新输入！\n");
+//    }
+//    return 0;
+//}
+
+
+int main()
+{
+
+
+}
