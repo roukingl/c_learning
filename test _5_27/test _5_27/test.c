e25fdbf364f752015ccb76b91f6ab6afb1ff1fb4
gitee_drh <2062806593@qq.com>
2022-05-27 21:26:02 +0800

    5_27的代码

---
+++ test _5_27/test _5_27/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//fun(int x, int y, int z)
+//{
+//	z = x * x + y * y;
+//}
+//void main()
+//{
+//	int a = 31;
+//	fun(5, 2, a);
+//	printf("%d", a);
+//}
+//
+//
+//int func(int a, int b)
+//{
+//	return(a + b);
+//}
+//void main()
+//{
+//	int x = 2, y = 5, z = 8, r;
+//	r = func(func(x, y), z);
+//	printf("%d\n", r);
+//}
+//
+//fun(fun1(a1, a2), (a3, a4), a5 = x + y);
+//fun(float x)
+//{
+//	float y;
+//	y = 3 * x - 4;
+//	return y;
+//}
